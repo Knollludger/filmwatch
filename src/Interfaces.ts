251b00f5ad36e67d6540a3931edf6a0efe21b39d
Sean Timm <seantimm3@aol.com>
2021-08-12 20:53:40 -0600

    feat: add interfaces file.

---
+++ src/Interfaces.ts
@@ -0,0 +1,16 @@
+export class UltimatePoint {
+  Ds: Array<number> = [];
+  Score = [];
+}
+
+export class Point {
+  x: number = -1;
+  y: number = -1;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export class Goal {}
