4f61e713036154502f2bcdd8f20fe7b94f458455
Sean Timm <seantimm3@aol.com>
2021-08-13 18:07:45 -0600

    feat: Update interfaces to prepare data.

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -6,11 +6,45 @@ export class UltimatePoint {
 export class Point {
   x: number = -1;
   y: number = -1;
-
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
   }
 }
 
-export class Goal {}
+export class Target extends Point {
+  completion: boolean = true;
+  // for if I ever track more than scores and Ds.
+  score: boolean = true;
+  constructor(x: number, y: number, completion: boolean) {
+    super(x, y);
+    this.completion = completion;
+    this.score = completion;
+  }
+}
+
+enum ThrowType {
+  Backhand,
+  Forehand,
+  Hammer,
+  Scoober,
+  PushPass,
+}
+
+enum ThrowAngle {
+  Flat,
+  OI,
+  IO,
+  NA,
+}
+export class Throw {
+  thrower: Point = new Point(0, 0);
+  target: Target = new Target(0, 0, false);
+  type: ThrowType = ThrowType.Backhand;
+  angle: ThrowAngle = ThrowAngle.Flat;
+
+  constructor(handler: Point, target: Target) {
+    this.thrower = handler;
+    this.target = target;
+  }
+}
